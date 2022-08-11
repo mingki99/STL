04b362b8cd9a715a506d81c60cc3fbd191e4be9f
ming <dkwnajsquf@naver.com>
2022-08-11 16:44:06 +0900

    array 연습코드 1,2,3

--- DataStructure/CPP_STL/main.cpp
+++ DataStructure/CPP_STL/main.cpp
@@ -1,30 +1,67 @@
 
 #include <iostream>
-#include "Vector.h"
 
+#include <array>
 
 using namespace std;
+
+// 예제 3번
+// 템플릿으로 사이즈 동적으로 받아준다.
+template<size_t size>
+void print(array<int, size> arr)
+{
+	for (auto ele : arr)
+		cout << ele << ", ";
+}
 
 
 int main()
 {
-	Vector<int> v;
+	//// 예제 1번
+	//array<int, 10> arr1;	// 크기가 10인 int 타입 배열 선언
 
-	for (int i = 0; i < 100; i++)
+	//arr1[0] = 1;	// 첫번째 원소를 1로 설정
+
+	//cout << "arr1의 첫번째 원소 " << arr1[0] << endl;
+
+
+	//array<int, 4> arr2 = {1, 2, 3, 4};
+
+	//cout << "arr2의 모든 원소" ;
+
+	//for (int i = 0; i < arr2.size(); i++)
+	//	cout << arr2[i] ;
+
+	//cout << endl;
+
+
+	//// 예제 2번
+	//// .at() 함수는 인덱스와 똑같이 기능한다 index = []
+	
+	//array<int, 4> arr3 = {1, 2, 3, 4};
+
+	//try
+	//{
+	//	cout << arr3.at(3) << endl; // 에러 아님
+	//	cout << arr3.at(4) << endl; // out_of_range 예외 발생
+	//}
+	//catch (const std::out_of_range& ex)
+	//{
+	//	cerr << ex.what() << endl;
+	//}
+
+	// 예제 3번
+	// 배열값 전달하기
+
+	array<int, 5> arr = {1, 2, 3, 4, 5};
+	// print(arr);
+	
+	for (auto it = arr.begin(); it != arr.end(); it++)
 	{
-		v.push_back(i);
-		cout << v.GetSize() << " " << v.GetCapacity() << endl;
+		auto element = (*it);
+		cout << element << " ";
 	}
 
-	for (int i = 0; i < v.GetSize(); i++)
-	{
-		cout << v[i] << endl;
-	}
 
-	cout << "-------------------------" << endl;
-
-	for (Vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-	{
-		cout << (*it) << endl;
-	}
+	return 0;
 }
